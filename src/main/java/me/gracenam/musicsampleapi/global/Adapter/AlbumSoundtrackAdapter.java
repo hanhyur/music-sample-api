c286769a754e11a819687a348e33aa06c08c9a31
gracenam <zeidgame@gmail.com>
2022-10-17 17:50:31 +0900

    feature : 리팩토링 중
    <description>
    - 리팩토링
    >> 불필요한 코드 제거 및 일관성 있는 코드로 리팩토링
    
    <issue-number>

--- src/main/java/me/gracenam/musicsampleapi/global/Adapter/AlbumSoundtrackAdapter.java
+++ src/main/java/me/gracenam/musicsampleapi/global/Adapter/AlbumSoundtrackAdapter.java
@@ -7,13 +7,11 @@ import me.gracenam.musicsampleapi.domain.albums.dto.response.AlbumDetailResponse
 import me.gracenam.musicsampleapi.domain.albums.dto.response.AlbumResponse;
 import me.gracenam.musicsampleapi.domain.albums.service.AlbumService;
 import me.gracenam.musicsampleapi.domain.soundtrack.dto.request.SoundtrackRequest;
-import me.gracenam.musicsampleapi.domain.soundtrack.dto.request.SoundtrackUpdateRequest;
 import me.gracenam.musicsampleapi.domain.soundtrack.dto.response.SoundtrackResponse;
 import me.gracenam.musicsampleapi.domain.soundtrack.service.SoundtrackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -25,7 +23,6 @@ public class AlbumSoundtrackAdapter {
 
     @Autowired
     private SoundtrackService soundtrackService;
-
 
     public AlbumResponse saveInfo(AlbumRequest albumReq) {
         log.info("albumReq = {}", albumReq);
@@ -63,8 +60,6 @@ public class AlbumSoundtrackAdapter {
     }
 
     public void deleteAlbumInfo(Long id) {
-        List<SoundtrackResponse> deleteList = soundtrackService.findSoundtracksByAlbumId(id);
-
         soundtrackService.deleteSoundtrack(id);
 
         albumService.deleteAlbum(id);
